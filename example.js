12b1f1b4da7e0bc01d4b88ef2866430e1e4e08d7
Surya Mattu <surya@suryamattu.com>
2019-12-08 18:31:57 -0500

    chore: gitignore update

--- example.js
+++ example.js
@@ -3,13 +3,13 @@ const { join } = require("path");
 // Lisst of devices you can emulate
 // https://github.com/puppeteer/puppeteer/blob/master/lib/DeviceDescriptors.js
 (async () => {
-  const URL = "veteransunited.com";
+  const URL = "nytimes.com";
   const inUrl = `http://${URL}`;
-  const outDir = join(__dirname, "example-dir");
+  const outDir = join(__dirname, "example-dir", URL);
   const result = await collector({
     inUrl,
     outDir,
-    numPages: 3,
+    numPages: 2,
     headless: true,
     captureHar: true
   });
