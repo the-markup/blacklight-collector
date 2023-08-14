c07c4797042f55c463040b321149ed4ae864530d
Bach Le <100985625+bachdumpling@users.noreply.github.com>
2023-08-14 16:20:23 -0400

    refactor fillForms timeout

--- src/pptr-utils/interaction-utils.ts
+++ src/pptr-utils/interaction-utils.ts
@@ -32,67 +32,79 @@ export const DEFAULT_INPUT_VALUES = {
     impp: 'xmpp:blacklight-headless@themarkup.org'
 };
 
-export const fillForms = async (page: Page) => {
-    try {
-        console.log('Checking for inputs on the page');
-        const elements = await page.$$('input');
-        console.log(`Found ${elements.length} input elements`);
-        let count = 0;
-        for (const el of elements) {
-            console.log(`Inspecting element ${count}`);
-            if (count > 100) {
-                break;
-            }
-            count += 1;
+export const fillForms = async (page: Page, timeout = 6000) => {
+    const timeoutPromise = new Promise((resolve) => {
+        setTimeout(() => {
+            console.log('Timeout reached. Exiting fillForms().');
+            resolve('Timeout'); // Resolve instead of reject
+        }, timeout);
+    });
 
-            const pHandle = await el.getProperty('type');
-            const pValue = await pHandle.jsonValue();
-            console.log(`Input is type ${pValue}`);
+    const fillPromise = async () => {
+        console.log('Entering fillPromise.');
+        try {
+            console.log('Checking for inputs on the page');
+            const elements = await page.$$('input');
+            console.log(`Found ${elements.length} input elements`);
+            let count = 0;
+            for (const el of elements) {
+                console.log(`Inspecting element ${count}`);
+                if (count > 100) {
+                    break;
+                }
+                count += 1;
 
-            const autoCompleteHandle = await el.getProperty('autocomplete');
-            const autoCompleteValue = (await autoCompleteHandle.jsonValue()) as string;
-            console.log(`Autocomplete attribute is: ${autoCompleteValue}`);
-            let autoCompleteKeys = [];
+                const pHandle = await el.getProperty('type');
+                const pValue = await pHandle.jsonValue();
+                console.log(`Input is type ${pValue}`);
 
-            console.log('Checking autocomplete value');
-            if (autoCompleteValue) {
-                if (autoCompleteValue.includes('cc-name')) {
-                    console.log('Autocomplete includes cc-name.');
-                    autoCompleteKeys = ['cc-name'];
-                } else {
-                    console.log('Autocomplete does not include cc-name.');
-                    autoCompleteKeys = Object.keys(DEFAULT_INPUT_VALUES).filter(k => (autoCompleteValue as string).includes(k));
+                const autoCompleteHandle = await el.getProperty('autocomplete');
+                const autoCompleteValue = (await autoCompleteHandle.jsonValue()) as string;
+                console.log(`Autocomplete attribute is: ${autoCompleteValue}`);
+                let autoCompleteKeys = [];
+
+                console.log('Checking autocomplete value');
+                if (autoCompleteValue) {
+                    if (autoCompleteValue.includes('cc-name')) {
+                        console.log('Autocomplete includes cc-name.');
+                        autoCompleteKeys = ['cc-name'];
+                    } else {
+                        console.log('Autocomplete does not include cc-name.');
+                        autoCompleteKeys = Object.keys(DEFAULT_INPUT_VALUES).filter(k => (autoCompleteValue as string).includes(k));
+                    }
+                }
+
+                if (pValue === 'submit' || pValue === 'hidden') {
+                    console.log('Type is either submit or hidden.');
+                    continue;
+                } else if (autoCompleteKeys.length > 0) {
+                    console.log('Autocomplete keys > 0');
+                    await el.focus();
+                    await page.keyboard.press('Tab', {
+                        delay: 100
+                    });
+                    await el.press('Backspace');
+                    await page.keyboard.type(DEFAULT_INPUT_VALUES[autoCompleteKeys[0] as string]);
+                } else if (Object.keys(DEFAULT_INPUT_VALUES).includes(pValue as string)) {
+                    console.log('Default input values includes pValue');
+                    await el.focus();
+                    await page.keyboard.press('Tab', {
+                        delay: 100
+                    });
+                    await el.press('Backspace');
+                    await page.keyboard.type(DEFAULT_INPUT_VALUES[pValue as string]);
+                    console.log(' ... done with test');
                 }
             }
-
-            if (pValue === 'submit' || pValue === 'hidden') {
-                console.log('Type is either submit or hidden.');
-                continue;
-            } else if (autoCompleteKeys.length > 0) {
-                console.log('Autocomplete keys > 0');
-                await el.focus();
-                await page.keyboard.press('Tab', {
-                    delay: 100
-                });
-                await el.press('Backspace');
-                await page.keyboard.type(DEFAULT_INPUT_VALUES[autoCompleteKeys[0] as string]);
-            } else if (Object.keys(DEFAULT_INPUT_VALUES).includes(pValue as string)) {
-                console.log('Default input values includes pValue');
-                await el.focus();
-                await page.keyboard.press('Tab', {
-                    delay: 100
-                });
-                await el.press('Backspace');
-                await page.keyboard.type(DEFAULT_INPUT_VALUES[pValue as string]);
-                console.log(' ... done with test');
-            }
-            await new Promise(resolve => setTimeout(resolve, 100));
-            console.log(' .... done');
+        } catch (error) {
+            console.log(error);
+        } finally {
+            console.log('Done with fillForms');
         }
-    } catch (error) {
-        console.log(error);
-    }
+    };
+    return await Promise.race([timeoutPromise, fillPromise()]);
 };
+
 export const autoScroll = async page => {
     await page.evaluate(async () => {
         return new Promise((resolve, reject) => {
