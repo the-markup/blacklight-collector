d09428bf1ba10f9e4ef97b1cf4909e31f5eabeef
Tomas Apodaca <546665+tmaybe@users.noreply.github.com>
2024-03-27 13:55:28 -0700

    revert changes to example and start checksite

--- example.ts
+++ example.ts
@@ -1,19 +1,10 @@
-import fs from 'fs';
-import { CollectorOptions, collect } from './src';
 import { KnownDevices } from 'puppeteer';
+import { CollectorOptions, collect } from './src';
 import { join } from 'path';
 
 (async () => {
+    const URL = process.argv.length > 2 ? process.argv[2] : 'example.com';
     const EMULATE_DEVICE = 'iPhone 13 Mini';
-    // first argument passed to the script is the url to test
-    const URL = process.argv.length >= 2 ? process.argv[2] : 'example.com';
-    // second argument is the output directory name; make sure it's filesystem-safe
-    const OUTDIR = process.argv.length >= 3 ? process.argv[3].replace(/[^a-z0-9]/gi, '_').toLowerCase() : 'demo-dir';
-    const OUTPATH = join(__dirname, OUTDIR);
-    // and create it if it doesn't exist
-    if (!fs.existsSync(OUTPATH)) {
-        fs.mkdirSync(OUTPATH);
-    }
 
     const config: CollectorOptions = {
         numPages: 1,
@@ -24,7 +15,7 @@ import { join } from 'path';
         //     viewport: {height: 1440, width: 800},
         //     userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
         // },
-        outDir: OUTPATH
+        outDir: join(__dirname, 'demo-dir')
     };
 
     console.log(`Beginning scan of ${URL}`);
