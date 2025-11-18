14c5ea7bff235aa6a308e7dc2ad7a4f00b43d7a8
FrankYiquan <147743539+FrankYiquan@users.noreply.github.com>
2025-11-17 22:14:39 -0500

    change example.ts back to its original state

--- example.ts
+++ example.ts
@@ -3,7 +3,7 @@ import { CollectorOptions, collect } from './src';
 import { join } from 'path';
 
 (async () => {
-    const URL = process.argv.length > 2 ? process.argv[2] : 'macys.com/';
+    const URL = process.argv.length > 2 ? process.argv[2] : 'themarkup.org';
     const EMULATE_DEVICE = 'iPhone 13 Mini';
 
     const config: CollectorOptions = {
